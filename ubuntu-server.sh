dd8f9b228a3791ebd71cee54a9e320361fc8c1bb
Oliver Steiner <osteiner@gmail.com>
2024-11-22 00:14:56 -0300

    Small change

--- ubuntu-server.sh
+++ ubuntu-server.sh
@@ -12,39 +12,40 @@ echo "Setting up Ubuntu Server"
 sudo apt update
 
 # Install zsh and plugins
-echo "Installing other tools"
+echo "\n================= Installing other tools ======================"
 sudo apt install eza stow btop neovim tmux tree zoxide unzip -y
 
-echo "Install zsh and plugins"
+echo "\n ================== Install zsh and plugins ======================"
 sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y
 
 # Install powerlevel10k
-echo "Installing powerlevel10k"
+echo "\n==================== Installing powerlevel10k ========================="
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 # Install fzf
-echo "Installing fzf"
+echo "\n ======================= Installing fzf ============================"
 sudo apt install fd-find
+mkdir ~/.local/bin
 ln -s $(which fdfind) ~/.local/bin/fd
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Install yazi
-echo "Installing yazi"
+echo "\n========================== Installing yazi ==========================="
 cd ~
 wget https://github.com/sxyazi/yazi/releases/download/v0.3.3/yazi-x86_64-unknown-linux-gnu.zip
 unzip yazi-x86_64-unknown-linux-gnu.zip
 mv ~/yazi-x86_64-unknown-linux-gnu/yazi ~/.local/bin
 
 # Install bat
-echo "Installing bat"
+echo "\n ============================ Installing bat ==========================="
 sudo apt install bat -y
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
 # Install dotfiles
-echo "Installing dotfiles"
+echo "\n============================ Installing dotfiles ============================"
 rm ~/.fzf.*
 rm ~/.zshrc
 cd ~/dotfiles/
@@ -53,6 +54,16 @@ stow cross-tmux
 stow cross-nvim
 stow cross-yazi
 stow cross-git
+
+# Remove heavy neovim plugins
+rm ~/.config/nvim/lua/plugins/telescope.lua
+rm ~/.config/nvim/lua/plugins/treesitter.lua
+rm ~/.config/nvim/lua/plugins/neo-tree.lua
+rm ~/.config/nvim/lua/plugins/lspconfig.lua
+rm ~/.config/nvim/lua/plugins/todo-comments.lua
+rm ~/.config/nvim/lua/plugins/conform.lua
+rm ~/.config/nvim/lua/plugins/trouble.lua
+rm ~/.config/nvim/lua/plugins/dashboard-nvim.lua
 
 # Change shell to zsh
 sudo passwd
