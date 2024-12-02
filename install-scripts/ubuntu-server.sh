de30ca2440d2c1eca7640eb6777a609ca0a07b64
Oliver Steiner <osteiner@gmail.com>
2024-12-02 10:32:56 -0300

    Separated comfigs by host.

--- install-scripts/ubuntu-server.sh
+++ install-scripts/ubuntu-server.sh
@@ -12,9 +12,6 @@ echo "Setting up Ubuntu Server"
 sudo apt update
 
 # Install zsh and plugins
-echo -e "================= Installing other tools ======================"
-sudo apt install eza stow btop neovim tmux tree zoxide unzip -y
-
 echo -e " ================== Install zsh and plugins ======================"
 sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y
 
@@ -22,7 +19,25 @@ sudo apt install zsh zsh-autosuggestions zsh-syntax-highlighting -y
 echo -e "==================== Installing powerlevel10k ========================="
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
-# Install fzf
+
+echo -e "================= Installing other tools ======================"
+sudo apt install stow btop tmux tree zoxide net-tools unzip git-diff -y
+
+echo -e "================= Installing eza ====================="
+sudo apt install gpg -y
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install eza -y
+
+echo -e "================= Installing neovim ===================="
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim -y
+
 echo -e "======================= Installing fzf ============================"
 sudo apt install fd-find
 mkdir -p ~/.local/bin
@@ -31,14 +46,12 @@ ln -s $(which fdfind) ~/.local/bin/fd
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-# Install yazi
 echo -e "========================== Installing yazi ==========================="
 cd ~
 wget https://github.com/sxyazi/yazi/releases/download/v0.3.3/yazi-x86_64-unknown-linux-gnu.zip
 unzip yazi-x86_64-unknown-linux-gnu.zip
 mv ~/yazi-x86_64-unknown-linux-gnu/yazi ~/.local/bin
 
-# Install bat
 echo -e "============================ Installing bat ==========================="
 sudo apt install bat -y
 ln -s /usr/bin/batcat ~/.local/bin/bat
@@ -54,16 +67,6 @@ stow cross-nvim
 stow cross-yazi
 stow cross-git
 stow cross-btop
-
-# Remove heavy neovim plugins
-rm ~/.config/nvim/lua/plugins/telescope.lua
-rm ~/.config/nvim/lua/plugins/treesitter.lua
-rm ~/.config/nvim/lua/plugins/neo-tree.lua
-rm ~/.config/nvim/lua/plugins/lspconfig.lua
-rm ~/.config/nvim/lua/plugins/todo-comments.lua
-rm ~/.config/nvim/lua/plugins/conform.lua
-rm ~/.config/nvim/lua/plugins/trouble.lua
-rm ~/.config/nvim/lua/plugins/dashboard-nvim.lua
 
 # Change sudoedit editor (manual)
 sudo update-alternatives --config editor
